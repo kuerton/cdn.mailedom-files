4708e6489d77945568993af0c84b680495239d62
Artem Kuerton <110241134+kuerton@users.noreply.github.com>
2024-03-09 23:29:12 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            images: [
+                // General
+                'img/main/general/background.svg',
+                'img/main/general/bot.svg',
+                'img/main/general/discord.svg',
+                'img/main/general/favicon.svg',
+                'img/main/general/mailedom.svg',
+                'img/main/general/telegram.svg',
+                'img/main/general/vk.svg',
+                'img/main/general/youtube.svg',
+                // hud
+                'img/main/hud/bankcard.svg',
+                'img/main/hud/cash.svg',
+                'img/main/hud/cloud.svg',
+                'img/main/hud/coin.svg',
+                'img/main/hud/tablet.svg',
+                // pay
+                'img/main/pay/mastercard.svg',
+                'img/main/pay/mir.svg',
+                'img/main/pay/visa.svg',
+            ],
+            searchQuery: ''
+        }
+    },
+    computed: {
+        filteredImages() {
+            return this.images.filter(image => image.includes(this.searchQuery));
+        }
+    },
+    methods: {
+        openImage() {
+            if (this.filteredImages.length > 0) {
+                window.open(`${this.filteredImages[0]}`, '_blank');
+                this.searchQuery = '';
+            }
+        }
+    }
+});
+
+app.mount('#app');
